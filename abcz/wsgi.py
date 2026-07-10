2515dc330315f929fa3fad205e0fc6993d21b88d
Saleh Alhadri <saleh@example.com>
2026-07-10 23:47:39 +0300

    Run migrations on Render startup

--- abcz/wsgi.py
+++ abcz/wsgi.py
@@ -14,3 +14,20 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abcz.settings')
 
 application = get_wsgi_application()
+
+
+def _env_truthy(name, default="false"):
+    return os.environ.get(name, default).strip().lower() in {"1", "true", "yes", "on"}
+
+
+def _run_startup_migrations_on_render():
+    default = os.environ.get("RENDER", "false")
+    if not _env_truthy("RUN_MIGRATIONS_ON_STARTUP", default):
+        return
+
+    from django.core.management import call_command
+
+    call_command("migrate", interactive=False, verbosity=1)
+
+
+_run_startup_migrations_on_render()
